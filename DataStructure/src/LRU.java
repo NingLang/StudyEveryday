7c783d363bb2e05f2fc3bf55c74883db02ef3b95
97749 <977492767@qq.com>
2021-03-30 09:00:16 +0800

    LRU名称修改

--- DataStructure/src/LRU.java
+++ DataStructure/src/LRU.java
@@ -2,13 +2,13 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
-public class LRU1<K, V> {
+public class LRU<K, V> {
     private final int MAX_CACHE_SIZE;
     private final float DEFAULT_LOAD_FACTORY = 0.75f;
 
     LinkedHashMap<K, V> map;
 
-    public LRU1(int cacheSize) {
+    public LRU(int cacheSize) {
         MAX_CACHE_SIZE = cacheSize;
         int capacity = (int)Math.ceil(MAX_CACHE_SIZE / DEFAULT_LOAD_FACTORY) + 1;
         /*
@@ -24,17 +24,17 @@ public class LRU1<K, V> {
     }
 
     public static void main(String[] args) {
-        LRU1<Integer, Integer> lru1 = new LRU1<>(5);
-        lru1.put(1, 1);
-        lru1.put(2, 2);
-        lru1.put(3, 3);
-        System.out.println(lru1);
-        lru1.get(1);
-        System.out.println(lru1);
-        lru1.put(4, 4);
-        lru1.put(5, 5);
-        lru1.put(6, 6);
-        System.out.println(lru1);
+        LRU<Integer, Integer> lru = new LRU<>(5);
+        lru.put(1, 1);
+        lru.put(2, 2);
+        lru.put(3, 3);
+        System.out.println(lru);
+        lru.get(1);
+        System.out.println(lru);
+        lru.put(4, 4);
+        lru.put(5, 5);
+        lru.put(6, 6);
+        System.out.println(lru);
     }
 
     public synchronized void put(K key, V value) {
